ec45ca6ec5c20f8fd19cce4d45d4f4cd8d973248
Melissa Miller <lis.d.miller@gmail.com>
2016-03-21 12:05:01 -0400

    Adds comments to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,11 +12,13 @@ angry = Emotion.create(emotion_name: 'Angry')
 
 happycorgi1 = Corgi.create(img_url: "http://i.imgur.com/tdavu17.gif", quote: "Walk tall!", emotion: happy)
 happycomment1 = Comment.create(text: "You are my inspiration!", commenter: 'Melissa', corgi: happycorgi1)
+happycomment2 = Comment.create(text: "Thanks to you, I have plans to climb a mountain! I'll be so tall!", commenter: 'Dream Big', corgi: happycorgi1)
+happycomment3 = Comment.create(text: 'Be careful not to fall!', commenter: 'Stubs McStumperson', corgi: happycorgi1)
 
 happycorgi2 = Corgi.create(img_url: "http://i.imgur.com/18f7bdG.gif", quote: "Be ready for anything!", emotion: happy)
 
 sadcorgi1 = Corgi.create(img_url: "http://i.imgur.com/wKjdA9G.jpg", quote: "I was meant for so much more than this.", emotion: sad)
 sadcorgi2 = Corgi.create(img_url: "http://i.imgur.com/tpVL1.jpg", quote: "I thought you loved me", emotion: sad)
 
-angrycorgi1 = Corgi.create(img_url: "http://i.imgur.com/JEAmgJX.jpg", quote: "This is NOT the park", emotion: angry)
+angrycorgi1 = Corgi.create(img_url: "http://i.imgur.com/JEAmgJX.jpg", quote: "This is NOT the dog park", emotion: angry)
 angrycorgi2 = Corgi.create(img_url: "http://i.imgur.com/RAFH7bf.gif", quote: "Cabbages!!!", emotion: angry)
