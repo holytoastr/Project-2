09190dedcb372ed434bd6476e543c8dab6df810e
Melissa Miller <lis.d.miller@gmail.com>
2016-03-22 13:41:57 -0400

    In case I break all the things

--- app/controllers/corgis_controller.rb
+++ app/controllers/corgis_controller.rb
@@ -4,5 +4,18 @@ class CorgisController < ApplicationController
   end
 
   def new
+    @emotion = Emotion.find(params[:format])
+    @corgi = Corgi.new
+  end
+
+  def create
+    @emotion = Emotion.find(params[:format])
+    @corgi = Corgi.create!(corgi_params)
+    redirect_to corgi_path(@corgi)
+  end
+
+  private
+  def corgi_params
+    params.require(:corgi).permit(:img_url, :quote, :emotion_id)
   end
 end
